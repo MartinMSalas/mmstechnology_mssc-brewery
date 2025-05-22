c70d52cc127be282caf6a23239d7b1ff3f497d8c
MartinMSalas <ingmartinmsalas@gmail.com>
2025-05-22 06:21:25 -0300

    fix<BeerServiceImpl>:Fix beer Type

--- mssc-brewery/mssc-brewery/src/main/java/com/mmstechnology/mssc_brewery/services/BeerService.java
+++ mssc-brewery/mssc-brewery/src/main/java/com/mmstechnology/mssc_brewery/services/BeerService.java
@@ -7,4 +7,10 @@ import java.util.UUID;
 public interface BeerService {
 
     BeerDto getBeerById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeerById(UUID beerId, BeerDto beerDto);
+
+    void deleteById(UUID beerId);
 }
--- mssc-brewery/mssc-brewery/src/main/java/com/mmstechnology/mssc_brewery/web/controller/BeerController.java
+++ mssc-brewery/mssc-brewery/src/main/java/com/mmstechnology/mssc_brewery/web/controller/BeerController.java
@@ -3,16 +3,16 @@ package com.mmstechnology.mssc_brewery.web.controller;
 import com.mmstechnology.mssc_brewery.services.BeerService;
 import com.mmstechnology.mssc_brewery.services.BeerServiceImpl;
 import com.mmstechnology.mssc_brewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @RequestMapping("api/v1/beer")
 @RestController
 public class BeerController {
@@ -38,5 +38,33 @@ public class BeerController {
         //return ResponseEntity.ok(beerDto);
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+        // Simulate saving the beer object
+        BeerDto savedBeer = beerService.saveNewBeer(beerDto);
+        log.info("Saved Beer: " + savedBeer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
+        return new ResponseEntity<>(savedBeer,headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<BeerDto> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        // Simulate updating the beer object
+        BeerDto updatedBeer = beerService.getBeerById(beerId);
+        log.info("Updated Beer: {}", updatedBeer);
+        return new ResponseEntity<>(updatedBeer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeerById(@PathVariable("beerId") UUID beerId) {
+        // Simulate deleting the beer object
+        beerService.deleteById(beerId);
+        log.info("Deleted Beer ID: {}", beerId);
     }
 }
